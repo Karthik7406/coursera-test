760bbf87f422f1d8894318fd40feb2581954101c
Karthik7406 <karthikkick5143@gmail.com>
2020-07-29 21:01:15 +0530

    Module 5 solution

--- mod4_solution/SpeakHello.js
+++ mod4_solution/SpeakHello.js
@@ -5,7 +5,6 @@
   var helloSpeaker={};
 
 
-
 // STEP 3: Create an object, called 'helloSpeaker' to which you will attach
 // the "speak" method and which you will expose to the global context
 // See Lecture 52, part 1
